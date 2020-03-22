40fb0d059de4e2d1b91fe6bab9314f3bb17fe53f
AdamBrownBB <adam.brown.betty@gmail.com>
2020-03-22 15:29:12 -0400

    first commit

---
+++ handler.js
@@ -0,0 +1,23 @@
+// 'use strict';
+
+const extFunc = require('./extFile.js')
+
+module.exports.hello = async event => {
+  const externalRes = extFunc()
+  console.log(externalRes)
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
